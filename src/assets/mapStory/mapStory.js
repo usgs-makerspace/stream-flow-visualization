14636cf985493bd581736d7245501de90c5c12c4
Megan Hines <mhines@usgs.gov>
2020-01-10 17:03:52 -0600

    Update story to include seasons

--- src/assets/mapStory/mapStory.js
+++ src/assets/mapStory/mapStory.js
@@ -14,7 +14,7 @@ export default {
             },
             "title": "Streamflow -- Water Year 2018",
             "content": "Annual runoff in the Nation's rivers and streams during water year 2018 (Oct 1, 2017 - Sep 30, 2018) was higher than the average annual runoff based on data from the last 89 years. Nationwide, the 2018 streamflow ranked 33rd highest out of the 89 years.",
-            "layersToHide": ["region_summary_2018","region_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
+            "layersToHide": ["region_summary_2018_plain","region_summary_2018","region_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
             "hiddenLayersToShow": ["state_summary_2018", "state_summary_2018_outline"]
         },
         {
@@ -31,7 +31,7 @@ export default {
             },
             "title": "Record lows",
             "content": "Record low streamflow levels were observed in Arizona; streamflow was much below normal in Colorado and New Mexico, too. Other states (Utah, Alaska, Kansas, Oregon) also had below normal streamflows.",
-            "layersToHide": ["region_summary_2018", "region_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
+            "layersToHide": ["region_summary_2018_plain","region_summary_2018", "region_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
             "hiddenLayersToShow": ["state_summary_2018", "state_summary_2018_outline"]
         },
         {
@@ -48,8 +48,76 @@ export default {
             },
             "title": "Record highs",
             "content": "Streamflow was above normal in Delaware, North Carolina, Nebraska, Wyoming, Tennessee, Indiana, Minnesota, DC, Kentucky and South Dakota. States with 'much above normal' streamflow included Iowa, Michigan, Maryland, Wisconsin, West Virginia, Ohio, Montana and Pennsylvania.",
-            "layersToHide": ["region_summary_2018", "region_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
+            "layersToHide": ["region_summary_2018_plain","region_summary_2018", "region_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
             "hiddenLayersToShow": ["state_summary_2018", "state_summary_2018_outline"]
+        },
+        {
+            "id": "seasonal-autumn",
+            "class": "",
+            "flyToCommands": {
+                "bearing": 0,
+                "center": [-97, 24],
+                "zoom": 3,
+                "pitch": 0,
+                "speed": 0.2,
+                "curve": 1,
+                "essential": true
+            },
+            "title": "Seasonal View - Autumn",
+            "content": "Streamflow was above normal in Delaware, North Carolina, Nebraska, Wyoming, Tennessee, Indiana, Minnesota, DC, Kentucky and South Dakota. States with 'much above normal' streamflow included Iowa, Michigan, Maryland, Wisconsin, West Virginia, Ohio, Montana and Pennsylvania.",
+            "layersToHide": ["region_summary_2018_plain","region_summary_2018", "region_summary_2018_outline", "state_summary_2018", "state_summary_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
+            "hiddenLayersToShow": ["state_autumn_2018", "state_autumn_2018_outline"]
+        },
+        {
+            "id": "seasonal-winter",
+            "class": "",
+            "flyToCommands": {
+                "bearing": 0,
+                "center": [-97, 24],
+                "zoom": 3,
+                "pitch": 0,
+                "speed": 0.2,
+                "curve": 1,
+                "essential": true
+            },
+            "title": "Seasonal View - Winter",
+            "content": "Streamflow was above normal in Delaware, North Carolina, Nebraska, Wyoming, Tennessee, Indiana, Minnesota, DC, Kentucky and South Dakota. States with 'much above normal' streamflow included Iowa, Michigan, Maryland, Wisconsin, West Virginia, Ohio, Montana and Pennsylvania.",
+            "layersToHide": ["region_summary_2018_plain","region_summary_2018", "region_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_summary_2018", "state_summary_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
+            "hiddenLayersToShow": ["state_winter_2018", "state_winter_2018_outline"]
+        },
+        {
+            "id": "seasonal-spring",
+            "class": "",
+            "flyToCommands": {
+                "bearing": 0,
+                "center": [-97, 24],
+                "zoom": 3,
+                "pitch": 0,
+                "speed": 0.2,
+                "curve": 1,
+                "essential": true
+            },
+            "title": "Seasonal View - Spring",
+            "content": "Streamflow was above normal in Delaware, North Carolina, Nebraska, Wyoming, Tennessee, Indiana, Minnesota, DC, Kentucky and South Dakota. States with 'much above normal' streamflow included Iowa, Michigan, Maryland, Wisconsin, West Virginia, Ohio, Montana and Pennsylvania.",
+            "layersToHide": ["region_summary_2018_plain","region_summary_2018", "region_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_summary_2018", "state_summary_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
+            "hiddenLayersToShow": ["state_spring_2018", "state_winter_spring_outline"]
+        },
+        {
+            "id": "seasonal-summer",
+            "class": "",
+            "flyToCommands": {
+                "bearing": 0,
+                "center": [-97, 24],
+                "zoom": 3,
+                "pitch": 0,
+                "speed": 0.2,
+                "curve": 1,
+                "essential": true
+            },
+            "title": "Seasonal View - Summer",
+            "content": "Streamflow was above normal in Delaware, North Carolina, Nebraska, Wyoming, Tennessee, Indiana, Minnesota, DC, Kentucky and South Dakota. States with 'much above normal' streamflow included Iowa, Michigan, Maryland, Wisconsin, West Virginia, Ohio, Montana and Pennsylvania.",
+            "layersToHide": ["region_summary_2018_plain","region_summary_2018", "region_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_summary_2018", "state_summary_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline"],
+            "hiddenLayersToShow": ["state_summer_2018", "state_summer_outline"]
         },
         {
             "id": "regional-patterns",
@@ -64,8 +132,8 @@ export default {
             },
             "title": "Regional Patterns",
             "content": "The US is divided up into water-resources regions that are based on surface topography and combined drainage areas of major rivers. These provide a different watershed-based way to depict the streamflow variation we just presented by state.",
-            "layersToHide": ["state_summary_2018", "state_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
-            "hiddenLayersToShow": []
+            "layersToHide": ["region_summary_2018","state_summary_2018", "state_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
+            "hiddenLayersToShow": ["region_summary_plain"]
         },
         {
             "id": "lower-colorado-region",
@@ -80,7 +148,7 @@ export default {
             },
             "title": "Lower Colorado Region",
             "content": "Streamflow was observed at much below normal levels in the Lower Colorado region.",
-            "layersToHide": ["state_summary_2018", "state_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
+            "layersToHide": ["region_summary_2018_plain","state_summary_2018", "state_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
             "hiddenLayersToShow": []
         },
         {
@@ -96,7 +164,7 @@ export default {
             },
             "title": "Regions below normal",
             "content": "The Upper Colorado, Rio Grande, Alaska and California regions also had below normal streamflow in 2018.",
-            "layersToHide": ["state_summary_2018", "state_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
+            "layersToHide": ["region_summary_2018_plain","state_summary_2018", "state_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
             "hiddenLayersToShow": []
         },
         {
@@ -112,7 +180,7 @@ export default {
             },
             "title": "Regions above normal",
             "content": "Tennessee, Upper Mississippi, Great Lakes and Mid-Atlantic regions all observed above normal streamflow in 2018.",
-            "layersToHide": ["state_summary_2018", "state_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
+            "layersToHide": ["region_summary_2018_plain","state_summary_2018", "state_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
             "hiddenLayersToShow": []
         },
         {
@@ -128,7 +196,7 @@ export default {
             },
             "title": "Regions WAyyyy above normal",
             "content": "The Ohio region experienced much above normal streamflows in 2018.",
-            "layersToHide": ["state_summary_2018", "state_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
+            "layersToHide": ["region_summary_2018_plain","state_summary_2018", "state_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
             "hiddenLayersToShow": []
         },
         {
@@ -144,7 +212,7 @@ export default {
             },
             "title": "Additional Information",
             "content": "The USGS operated a nationwide network of more than 8,200 streamgages in 2018, and almost all USGS streamgages are operated in real time. Current information derived from these streamgages is available at https://waterwatch.usgs.gov. Tables of data that summarize historical streamflow conditions by State, expressed as runoff, beginning in water year 1901, can be accessed at https://waterwatch.usgs.gov/?id=statesum. These tables are updated every few months to reflect the most current streamflow data.",
-            "layersToHide": ["state_summary_2018", "state_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
+            "layersToHide": ["region_summary_2018_plain","state_summary_2018", "state_summary_2018_outline", "state_autumn_2018", "state_autumn_2018_outline", "state_winter_2018", "state_winter_2018_outline", "state_spring_2018", "state_spring_2018_outline", "state_summer_2018", "state_summer_2018_outline"],
             "hiddenLayersToShow": []
         }
     ]
